375de40aab7b9ac4bb6cb999c1fde69cc607f1a7
Jason Raimondi <jason@raimondi.us>
2020-04-01 22:55:19 -0700

    Add passwd flag

--- backup_full.sh
+++ backup_full.sh
@@ -3,7 +3,7 @@
 echo "Starting full sync of $GMVAULT_EMAIL_ADDRESS."
 echo "Report will be sent to $GMVAULT_SEND_REPORTS_TO."
 
-gmvault sync -d /data $GMVAULT_OPTIONS $GMVAULT_EMAIL_ADDRESS 2>&1 \
+gmvault sync --passwd -d /data $GMVAULT_OPTIONS $GMVAULT_EMAIL_ADDRESS 2>&1 \
 	| tee /data/${GMVAULT_EMAIL_ADDRESS}_full.log \
 	| mail -s "Mail Backup (full) | $GMVAULT_EMAIL_ADDRESS | `date +'%Y-%m-%d %r %Z'`" $GMVAULT_SEND_REPORTS_TO
 
